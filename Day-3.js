65b83431fc643ecd63a8750e70808de66c86b96d
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-01-31 16:24:28 +0500

    Updated Day 3

--- Day-3.js
+++ Day-3.js
@@ -1,30 +1,32 @@
 /****************************Day 3*******************************/
 /*****Question 01******/
 //Write a function to count the frequency of each character in a string.
-function countFrequency(string) {
+function countFrequency1(string) {
   let frequency = {};
   for (let character of string) {
-    if (frequency[character]) {
-      frequency[character]++;
-    } else {
-      frequency[character] = 1;
-    }
+    frequency[character] = (frequency[character] || 0) + 1;
   }
   return frequency;
 }
-console.log(countFrequency("mehak")); //{ m: 1, e: 1, h: 1, a: 1, k: 1 }
-console.log(countFrequency("tayyab")); //{ t: 1, a: 2, y: 2, b: 1 }
+console.log(countFrequency1("mehak")); //{ m: 1, e: 1, h: 1, a: 1, k: 1 }
+console.log(countFrequency1("tayyab")); //{ t: 1, a: 2, y: 2, b: 1 }
+//alternate method using reduce
+function countFrequency2(string) {
+  return string.split("").reduce((frequency, char) => {
+    frequency[char] = (frequency[char] || 0) + 1;
+    return frequency;
+  }, {});
+}
+console.log(countFrequency2("mehak")); //{ m: 1, e: 1, h: 1, a: 1, k: 1 }
+
+//
 
 /*****Question 02******/
 //Write a function to find the most frequent element in an array.
-function checkMostFrequentElement(array) {
+function checkMostFrequentElement1(array) {
   let frequentElement = {};
   for (let element of array) {
-    if (frequentElement[element]) {
-      frequentElement[element]++;
-    } else {
-      frequentElement[element] = 1;
-    }
+    frequentElement[element] = (frequentElement[element] || 0) + 1;
   }
   //looping through it to find the element with the highest frequency.
   let maxCount = 0;
@@ -39,5 +41,19 @@ function checkMostFrequentElement(array) {
   }
   return mostFrequentElement;
 }
-console.log(checkMostFrequentElement([1, 2, 1, 2, 1])); //[ 1 ]
-console.log(checkMostFrequentElement([1, 1, 2, 2, 3, 3, 4, 4, 1, 3, 5])); //[ 1, 3 ]
+console.log(checkMostFrequentElement1([1, 2, 1, 2, 1])); //[ 1 ]
+console.log(checkMostFrequentElement1([1, 1, 2, 3, 3, 4, 4, 1, 3])); //[ 1, 3 ]
+//alternate method using reduce
+function checkMostFrequentElement2(arr) {
+  const frequencyMap = arr.reduce((map, num) => {
+    map[num] = (map[num] || 0) + 1;
+    return map;
+  }, {});
+
+  const maxFrequency = Math.max(...Object.values(frequencyMap));
+  return Object.keys(frequencyMap).filter(
+    (key) => frequencyMap[key] === maxFrequency
+  );
+}
+
+console.log(checkMostFrequentElement2([1, 1, 2, 3, 4, 5, 6, 5, 6, 5, 6, 7])); // [ '5', '6' ]
