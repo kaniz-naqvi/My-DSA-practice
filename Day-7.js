fb5f783dbab70549d9d285eff8878d1cdd193a37
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-02-08 15:55:05 +0500

    added day 7

---
+++ Day-7.js
@@ -0,0 +1,44 @@
+/****************************Day 7*******************************/
+/*****Question 01******/
+//Write a function to find the maximum subarray sum (Kadane’s Algorithm) and analyze its time and space complexity.
+//I am not familiar with Kadane’s Algorithm and do not know how can I make subarrays of my array.
+
+function maxSubarraySum(arr) {
+  let maxEnding = arr[0];
+  let maxSoFar = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    maxEnding = Math.max(arr[i], arr[i] + maxEnding);
+    maxSoFar = Math.max(maxEnding, maxSoFar);
+  }
+  return maxSoFar;
+}
+//The time complexity is O(n) (due to the loop and Math.max), and the space complexity is O(1).
+// const num = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubarraySum(num)); // Output: 6 (subarray [4, -1, 2, 1])
+
+/*****Question 02******/
+//Write a function to check if a string is a palindrome and analyze its time and space complexity.
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+//The time complexity is O(n) (due to the loop and join operation), and the space complexity is O(1).
+console.log(isPalindrome("mehak")); // output: false
+console.log(isPalindrome("level")); //output: true
+
+
+
+
+
+
+
+
+
