d255f8f688789d02aa6e93ca71febd373ef1d672
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-02-10 19:31:26 +0500

    update day 9

--- Day-9.js
+++ Day-9.js
@@ -1,39 +1,57 @@
 /****************************Day 9*******************************/
 /*****Question 01******/
-//Write a function to implement Bubble Sort and test it on the following array:
-//const arr = [5, 3, 8, 4, 6];
-function bubbleSort(arr) {
+//Write a function to implement Insertion Sort and test it on the following array:
+//const arr = [4, 3, 2, 10, 12, 1, 5, 6];
+function insertionSort(arr) {
   let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    let isSwap = false;
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        isSwap = true;
-      }
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    if (!isSwap) {
-      break;
-    }
+    arr[j + 1] = key;
   }
   return arr;
 }
-console.log(bubbleSort([5, 3, 8, 4, 6])); // output: [ 3, 4, 5, 6, 8 ]
+// output: [1, 2,  3,  4, 5, 6, 10, 12]
+console.log(insertionSort([4, 3, 2, 10, 12, 1, 5, 6]));
 
 /*****Question 02******/
-//Write a function to implement Selection Sort and test it on the following array:
-//const arr = [10, 7, 8, 9, 1, 5];
-function selectionSort(arr) {
-  let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-  }
-  return arr;
+//Write a function to implement Merge Sort and test it on the following array:
+//const arr = [12, 11, 13, 5, 6, 7];
+function mergeSort(arr) {
+  //base case
+  if (arr.length <= 1) return arr;
+  //recursive case
+  let mid = Math.floor(arr.length / 2);
+  let right = mergeSort(arr.slice(mid));
+  let left = mergeSort(arr.slice(0, mid));
+  //shorting
+  return merge(left, right);
 }
-console.log(selectionSort([10, 7, 8, 9, 1, 5]));
+function merge(left, right) {
+  let result = [],
+    i = 0,
+    j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  //to handel edge cases
+  result = result.concat(left.slice(i)).concat(right.slice(j));
+  return result;
+}
+console.log(mergeSort([12, 11, 13, 5, 6, 7]));
+/****************************Day 10*******************************/
+/*****Question 01******/
+//Write a function to implement Quick Sort and test it on the following array:
+//const arr = [3, 6, 8, 10, 1, 2, 1];
+/*****Question 02******/
+//Compare the performance of Merge Sort and Quick Sort on a large dataset (e.g., an array of 10,000 random numbers). Which one is faster? Why?
