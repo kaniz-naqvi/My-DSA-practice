87f1e5a3a93e609fa04fe30d1a320850623546a4
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-03-11 08:09:17 +0500

    update

--- Day-17.js
+++ Day-17.js
@@ -1,5 +1,5 @@
 /*******************Day 17************************/
-/******* Question 2********/
+/******* Question 1********/
 //Solve the Valid Parentheses problem for the following inputs:
 function isValid(s) {
   let stack = [];
@@ -22,3 +22,42 @@ function isValid(s) {
 console.log(isValid("({[]})")); // true
 console.log(isValid("([)]")); // false
 console.log(isValid("{{{{")); // false
+/******* Question 2********/
+//Extend the Min Stack implementation to handle duplicate minimum values correctly.
+class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+  push(value) {
+    this.stack.push(value);
+    if (
+      this.minStack.length === 0 ||
+      value <= this.minStack[this.minStack.length - 1]
+    ) {
+      this.minStack.push(value);
+    }
+  }
+  top() {
+    if (this.stack.length === 0) return null;
+    return this.stack[this.stack.length - 1];
+  }
+  pop() {
+    const poppedValue = this.stack.pop();
+    if (poppedValue === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+  }
+  getMin() {
+    if (this.stack.length === 0) return null;
+    return this.minStack[this.minStack.length - 1];
+  }
+}
+const minStack = new MinStack();
+minStack.push(5);
+minStack.push(8);
+minStack.push(1);
+minStack.push(1);
+minStack.push(9);
+console.log(minStack.top());
+console.log(minStack.getMin());
