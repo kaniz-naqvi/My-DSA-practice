c85e1905ef0b86c4db253e28c957768ae0dfb8d5
Mehak <mehak313naqvi@gmail.com>
2025-04-18 10:54:12 +0500

    update Day-1

--- Day1-10/Day1-Arrays.cpp
+++ Day1-10/Day1-Arrays.cpp
@@ -1,27 +1,42 @@
-#include <iostream>
 #include <climits>
-
+#include <iostream>
 using namespace std;
+
 int linearSearch(int arr[], int size, int target);
+void reverseArray(int array[], int size);
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << arr[i];
+    if (i != size - 1) {
+      cout << ',';
+    }
+  }
+  cout << endl;
+}
 int main() {
-  /*********** Qusetion 1 *************/
+  /*********** Question 1 *************/
   // Find smallest number in an array.
   int arr[] = {5, 6, 7, 8, 3, -1, -5, 2, 9};
-  int smallNum = INT_MAX;
   const int arrSize = sizeof(arr) / sizeof(int);
+  // print array
+  printArray(arr, arrSize);
+  int smallNum = INT_MAX;
+  int smallestNumIndex = 0;
   for (int i = 0; i < arrSize; i++) {
     if (arr[i] < smallNum) {
       smallNum = arr[i];
+      smallestNumIndex = i;
     }
     // shorter pre-built method
     // smallNum=min(arr[i], smallNum);
-    cout << arr[i] << endl;
   }
-  cout << "Smallest number is: " << smallNum << endl;
-  /*********** Qusetion 2 *************/
+  cout << "Smallest number is: " << smallNum << " on index " << smallestNumIndex
+       << endl;
+
+  /*********** Question 2 *************/
   // Find largest number in an array.
   int largestNum = INT_MIN;
-  /*********** Qusetion 3 *************/
+  /*********** Question 3 *************/
   // Print index instead of number.
   int largestNumIndex = 0;
   for (int i = 0; i < arrSize; i++) {
@@ -36,12 +51,15 @@ int main() {
        << endl;
 
   // Linear Search In array -> Q-4
-  cout << "Target 7 found at index " << linearSearch(arr, arrSize, 7);
-
+  cout << "Target 7 found at index " << linearSearch(arr, arrSize, 7) << endl;
+  // Reverse an Array
+  reverseArray(arr, arrSize);
+  // print array
+  printArray(arr, arrSize);
   return 0;
 }
-/*********** Qusetion 4 *************/
-//Linear Search
+/*********** Question 4 *************/
+// Linear Search
 int linearSearch(int arr[], int size, int target) {
   for (int i = 0; i < size; i++) {
     if (target == arr[i]) {
@@ -49,4 +67,14 @@ int linearSearch(int arr[], int size, int target) {
     }
   }
   return -1;
+}
+/*********** Question 5 *************/
+// Reverse an Array
+void reverseArray(int array[], int size) {
+  int start = 0, end = size - 1;
+  while (start < end) {
+    swap(array[start], array[end]);
+    start++;
+    end--;
+  }
 }
